a6f067584bec07f75b646844e11c84becb14b945
SidsOnIt <feerthespeer@gmail.com>
2026-06-26 20:45:32 -0700

    Update input.rs
    (refac) cleaned structure for better flow and readability

--- src/input.rs
+++ src/input.rs
@@ -1,13 +1,20 @@
 use std::io::{self, Write};
 
 pub fn input(prompt: &str) -> String {
-    print!("{}", prompt);
-    io::stdout().flush().expect("failed to flush std-out");
+    
+    print!("{prompt}");
+    
+    io::stdout()
+        .flush()
+        .expect("failed to flush std-out");
 
     let mut response = String::new();
+    
     io::stdin()
         .read_line(&mut response)
         .expect("failed to read line");
 
-    response.trim_end().to_string()
+    response
+        .trim_end()
+        .to_string()
 }
